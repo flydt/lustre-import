a45e62ee35427c61e1b2c99f0bb82ef04c92d3ef
qingyun_fu <qingyun_fu@qq.com>
2024-07-09 11:04:38 +0800

    add script to import all files descripted in list file path

---
+++ lhsm_load.sh
@@ -0,0 +1,56 @@
+#!/usr/bin/bash
+#
+# script to import bucket to lustre directory
+# s3_importx.sh
+
+if [ $# -lt 2 ];
+then
+    echo ${BASH_SOURCE} LUSTRE_IMPORT_PATH IMPORT_LIST_PATH IMPORT_BATCH_SIZE\(optional\)
+    exit 1
+fi
+
+lustre_mnt_path=$1
+import_list_path=$2
+
+# import batch default size is 1000 items
+batch_size=1000
+if [ $# -eq 3 ];
+then
+	batch_size=$3
+fi
+
+echo import files to lustre to path "$lustre_mnt_path" with list file path "$import_list_path" and batch size "$batch_size"
+
+loop_idx=1
+task_done=0
+
+run_task:
+
+echo loop "$loop_idx" start
+date
+
+list_num=`find $import_list_path -type f | wc -l`
+if [ $list_num -eq 0 ];
+then
+    echo no list file to process in directory "$import_list_path"
+    task_done=1
+else
+    find $import_list_path -type f -print0 | xargs -0 -n 1 -P 8 -I {} /opt/tgstor/bin/lhsm_import $lustre_mnt_path {} $batch_size
+fi
+
+echo loop "$loop_idx" finished
+
+if [ $task_done -eq 1 ];
+then
+    echo import files to lustre to path "$lustre_mnt_path" with list file path "$import_list_path" finished
+else
+    date
+    echo error happend in import, try again
+    sleep 5
+    goto run_task
+fi
+
+date
+exit 0
+~
+
